21ebff1f7763a4f25af6e10c0eb5058f611d46a8
Jeff Wolfram <jeffwolfram@gmail.com>
2016-12-07 09:45:01 -0800

    Just made some repairs

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,27 @@
+(function() {
+    function SongPlayer() {
+        var SongPlayer = {};
+        var currentSong = null;
+        var currentBuzzObject = null;
+        
+        SongPlayer.play = function(song) {
+            if(currentSong !== song){
+                if(currentBuzzObject){
+                    currentBuzzObject.stop();
+                }
+           
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload:true
+            });
+            currentSong = song;
+            currentBuzzObject.play();
+            }
+        };
+    return SongPlayer;
+    }
+    angular
+    .module('blocJams')
+    .factory('SongPlayer', SongPlayer);
+    
+})();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -15,7 +15,7 @@
          .state('album',  {
                 url: '/album',
                 controller: 'AlbumCtrl as album',
-                templateUrl: '/templates/album/html'
+                templateUrl: '/templates/album.html'
          })
          
          .state('collection', {
